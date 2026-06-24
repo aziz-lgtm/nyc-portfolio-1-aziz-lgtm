e7b361656f2a03ef70184adf67bd75336bfc2c00
FAIZAL <azizahmad0208@gmail.com>
2026-06-24 07:46:41 +0700

    start fixing PortfoliosSection.tsx

--- portfolio-1/src/components/PortfoliosSection.tsx
+++ portfolio-1/src/components/PortfoliosSection.tsx
@@ -1,18 +1,23 @@
 import { portfolio } from "@/data/portfolio";
 import Image from "next/image";
+import { Button } from "./ui/button";
+import { ArrowRight } from "lucide-react";
 
 export default function PortfoliosSection() {
     return(
-        <section>
+        <section className="flex flex-col items-start py-10 px-4 gap-6 w-[393px] h-[1767px] flex-none order-4 grow-0 relative left-1/2 top-0 -translate-x-1/2 bg-pink-300">
             {/* main box 1: box 1, box 2 */}
-            <div>
+            <div className="flex flex-col items-start gap-6 w-[361px] h-[210px] flex-none order-0 self-stretch grow-0">
                 {/* box 1: title & subtitle */}
-                <div>
-                    <h2></h2>
-                    <p></p>
+                <div className="flex flex-col items-start gap-2 w-[361px] h-[130px] flex-none order-0 self-stretch grow-0">
+                    <h2 className="w-[361px] h-[38px] font-['Montserrat'] font-bold text-[28px] leading-[38px] tracking-[-0.02em] text-[#0A0D12] flex-none order-0 self-stretch grow-0">{portfolio.title}</h2>
+                    <p className="w-[361px] h-[84px] font-['Montserrat'] font-medium text-[14px] leading-[28px] text-[#0A0D12] flex-none order-1 self-stretch grow-0">{portfolio.subtitle}</p>
                 </div>
-                {/* box 2: the card 'more' */}
-                <div></div>
+                {/* box 2: p 'See All', button 'more' */}
+                <div className="box-border flex flex-row justify-center items-center py-2 pl-4 pr-2 gap-2 w-[361px] h-14 border border-[#D5D7DA] rounded-full flex-none order-1 self-stretch grow-0">
+                    <p className="w-[293px] h-[30px] font-['Montserrat'] font-semibold text-[16px] leading-[30px] text-[#0A0D12] flex-none order-0 grow">{portfolio.seeAllCard}</p>
+                    <Button className="flex flex-row items-center p-2 gap-2 w-9 h-9 bg-[#0A0D12] rounded-full flex-none order-1 grow-0"><div className="w-[20px] h-[20px] flex items-center self-center justify-center"><ArrowRight className="w-[19.67px]! h-[19.67px]!" /></div></Button>
+                </div>
             </div>
             {/* main box 2: portfolio 1, portfolio 2, portfolio 3 */}
             <div>
